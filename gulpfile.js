220b14db6662a488a65f3f4421dc4db4870ff8dd
KingOneYan <kingoneyan@canway.net>
2022-04-27 18:19:44 +0800

    feat:gulp js文件压缩+重命名test

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp')
 var concat = require('gulp-concat')
-
+var rename = require('gulp-rename')
+var uglify = require('gulp-uglify')
 gulp.task('testConcat', function() {
     return gulp.src('./src/js/**/*.js')
         .pipe(concat('all.js'))
-        .pipe(gulp.dest('./dist/'));
+        .pipe(gulp.dest('./dist/'))
+        .pipe(uglify()) //压缩文件
+        .pipe(rename({suffix:'.min'})) //更改后缀
+        .pipe(gulp.dest('./dist/'))
 });
--- src/js/test2.js
+++ src/js/test2.js
@@ -1,3 +1,3 @@
 (function (){
     console.log('this is test2')
-})
+})()
